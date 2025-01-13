6a62c49a07f918603f75ed0c4a21d9be15ada908
karthika <55327368+karthika1607@users.noreply.github.com>
2025-01-13 17:02:36 +0530

    inital setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,86 @@
-import React, { useState } from "react";
-import SideMenu from "./SideMenu";
-import TabsPanel from "./TabsPanel";
-import { Box } from "@mui/material";
+// import React, { useState } from "react";
+// import SideMenu from "./SideMenu";
+// import TabsPanel from "./TabsPanel";
+// import { Box } from "@mui/material";
 
-function App() {
-  const [activeTabs, setActiveTabs] = useState([]);
+// function App() {
+//   const [activeTabs, setActiveTabs] = useState([]);
 
-  const addTab = (tabId, label) => {
-    if (!activeTabs.find((tab) => tab.id === tabId)) {
-      setActiveTabs((prevTabs) => [...prevTabs, { id: tabId, label }]);
-    }
-    setActiveTab(tabId);
-  };
+//   const addTab = (tabId, label) => {
+//     if (!activeTabs.find((tab) => tab.id === tabId)) {
+//       setActiveTabs((prevTabs) => [...prevTabs, { id: tabId, label }]);
+//     }
+//     setActiveTab(tabId);
+//   };
 
-  const removeTab = (tabId) => {
-    setActiveTabs((prevTabs) => prevTabs.filter((tab) => tab.id !== tabId));
-    if (activeTabs.length > 1 && activeTabs[0]?.id === tabId) {
-      setActiveTab(activeTabs[1]?.id);
-    }
-  };
+//   const removeTab = (tabId) => {
+//     setActiveTabs((prevTabs) => prevTabs.filter((tab) => tab.id !== tabId));
+//     if (activeTabs.length > 1 && activeTabs[0]?.id === tabId) {
+//       setActiveTab(activeTabs[1]?.id);
+//     }
+//   };
 
-  const [activeTab, setActiveTab] = useState(null);
+//   const [activeTab, setActiveTab] = useState(null);
 
+//   return (
+//     <Box sx={{ display: "flex", height: "100vh" }}>
+//       <SideMenu addTab={addTab} />
+//       <TabsPanel
+//         activeTabs={activeTabs}
+//         activeTab={activeTab}
+//         setActiveTab={setActiveTab}
+//         removeTab={removeTab}
+//       />
+//     </Box>
+//   );
+// }
+
+// export default App;
+
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { adminRoutes, authRoutes } from "./routes/index";
+import LoginLayout from "./layout/Login";
+import { Fragment } from "react";
+
+function AdminRoutes() {
   return (
-    <Box sx={{ display: "flex", height: "100vh" }}>
-      <SideMenu addTab={addTab} />
-      <TabsPanel
-        activeTabs={activeTabs}
-        activeTab={activeTab}
-        setActiveTab={setActiveTab}
-        removeTab={removeTab}
-      />
-    </Box>
+    <Fragment>
+      <Routes>
+        <Route path="*" element={<div>Not Found</div>} />
+        {authRoutes &&
+          authRoutes.map((route, key) => (
+            <Route
+              path={route.path}
+              element={
+                <LoginLayout>
+                  <route.component />
+                </LoginLayout>
+              }
+              key={key}
+            />
+          ))}
+      </Routes>
+    </Fragment>
   );
 }
 
-export default App;
+
+
+
+
+const MainApp = () => (
+  <BrowserRouter>
+    <Routes>
+      {/* <Route path="/" element={<Login />} /> */}
+      {/* <Route path="/test" element={<Test />} /> */}
+      <Route path="admin/*" element={<AdminRoutes />} />
+      {/* <Route path="distributor/*" element={<DistributorRoutes />} />
+      <Route path="customer/*" element={<CustomerRoutes />} /> */}
+      <Route path="*" element={<div>Not Found</div>} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default MainApp;
---
+++ src/routes/index.jsx
@@ -0,0 +1,29 @@
+////Admin Routes
+
+import Login from "../pages/login/login";
+
+
+
+// const adminRoutes = [
+//   {
+//     path: "dashboard",
+//     component: Dashboard,
+//   },
+//   {
+//     path: "add-plan",
+//     component: AddPlan,
+//   },
+
+
+// ];
+
+const authRoutes = [
+  {
+    path: "/",
+    component: Login,
+  },
+]
+
+
+
+export {  authRoutes };
